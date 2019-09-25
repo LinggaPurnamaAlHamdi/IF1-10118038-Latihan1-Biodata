cd0b2ae2c0b4aa409ef0ca91203100863adaf751
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:23:14 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan1-Biodata/src/if1/pkg10118038/latihan1/biodata/IF110118038Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan1.biodata;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * biodata
+ */ 
+
+ 
+public class IF110118038Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA" + "\n" + "---------------------------" );
+        System.out.println("NAMA\t\t: Lingga Purnama Al Hamdi");
+        System.out.println("ALAMAT\t\t: Jl. Muara Takus Raya R55 ");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 08 Agustus 2000");
+        System.out.println("----------------------------");
+        System.out.println("HOBI\t\t: Mendengarkan Musik,Bermain Game");
+        System.out.println("MAKES\t\t: Soto Ayam");
+        System.out.println("MIKES\t\t: Es Jeruk");
+    }
+    
+}
